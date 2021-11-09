024e5a6ad4d926bc2a94348b1a5f330e791aac06
Marjica Watson-Santos <marjicawatson-santos@Marjicas-MBP.attlocal.net>
2021-11-09 15:02:45 -0500

    Need Help

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
-let genreSearch = document.getElementsByClassName("genreList");
+let genreSearch = document.querySelector(".genrelist");
+
+///User selects genre from drop down 
+
+
+
+
+
+
+///Then clicks "Go" button, every time go is pressed 
+//it brings. another random artist in the genre
+
+
+
+
+
+
+
+/// User is returned image, link, and name for chosen genre
+
 
