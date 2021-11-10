9ca5e98092b62e49219668f021ac39a6fccd393d
Marjica Watson-Santos <marjicawatson-santos@Marjicas-MBP.attlocal.net>
2021-11-10 15:30:42 -0500

    Go easy

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
-let genreSearch = document.querySelector(".genrelist");
+//let genreSearch = document.querySelector(".genrelist");
 
-console.log(axios.get("https://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&artist=&api_key=aed2c1389af55ee448291a0b7eeaddfa&format=json"));
-   // .then((res) => {
-  //      console.log(res);
-  //  });
+const artistDiv = document.querySelector(".artist");
+console.log(artistDiv);
+
+function displayArtist(artist) {
+ console.log(artist);
+
+ let div = document.createElement("div")
+ let h3 = document.createElement("h3")
+ h3.innerText = artist[0];
+ div.appendChild(h3);
+ artistDiv.appendChild(div);
+
+ //append to artist div
+ //append to
+}
+
+///Request to get the data
+axios.get("https://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&artist=&api_key=aed2c1389af55ee448291a0b7eeaddfa&format=json")
+   .then((res) => {
+    const artist = res.data.tracks;
+        displayArtist(artist);
+  })
+  .catch((error)=> {
+      console.log(error);
+  })
+  .finally(() => {
+      console.log("DONE");
+  });
+
+
 ///User selects genre from drop down 
 
 
